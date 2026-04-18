cd2c399cffb124918097085f96083d0bce593098
LukaBabunadze <luka.babunadze.1@iliauni.edu.ge>
2026-04-18 14:38:26 +0400

    I've refactored navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,30 +1,27 @@
+import Link from "next/link";
 import styles from "./Navbar.module.css";
+
+const NAV_LIST = [
+  { id: 1, title: "Main", url: "/" },
+  { id: 2, title: "Cart", url: "/cart" },
+  { id: 3, title: "Profile", url: "/profile" },
+  { id: 4, title: "Contact", url: "/contact" },
+];
+
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <h1>Navbar</h1>
-      <ul className={styles.listWrapper}>
-        <li>Home</li>
-        <li>Contact</li>
-        <li>Cart</li>
-        <li>Profile</li>
-      </ul>
+      {NAV_LIST.map((item) => (
+        <div key={item.id}>
+          <Link href={item.url}>
+            <h3>{item.title}</h3>
+          </Link>
+        </div>
+      ))}
+      <div className={styles.listWrapper}></div>
     </nav>
   );
 };
 
 export default Navbar;
-
-// export default function Navbar () {
-//   return (
-//     <nav>
-//       <h1>Navbar</h1>
-//       <ul>
-//         <li>Home</li>
-//         <li>Contact</li>
-//         <li>Cart</li>
-//         <li>Profile</li>
-//       </ul>
-//     </nav>
-//   );
-// };
