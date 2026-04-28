c29208d001abab0c1bc3d567a599885dd593c882
LukaBabunadze <luka.babunadze.1@iliauni.edu.ge>
2026-04-28 20:48:13 +0400

    profile | Server Side Fetching

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,5 +1,12 @@
-function page() {
+const page = async () => {
+  const response = await fetch("https://fakestoreapi.com/users/3");
+  const user = await response.json();
+
+  if (!user) {
+    return <div>მოხდა შეცდომა!</div>;
+  }
+
   return <div>Profile</div>;
-}
+};
 
 export default page;
